2909e5dacd0fc52a4402d0dbb5471d0f5b31d18e
Yuan Jing Vincent Yan <kaikulimu@yahoo.com.hk>
2017-04-04 09:18:55 -0400

    MNIST: add params.py

---
+++ tensorflow_MNIST/params.py
@@ -0,0 +1,4 @@
+MAX_BITS_IMAGE_SIZE = 24            # maximum number of bits used to represent the size of an image
+IMAGE_SIZE_PACKET_TAG = "Arnold"
+LEN_IMAGE_SIZE_PACKET_TAG = 6
+MAX_PACKET_SIZE = 1460              # maximum UDP packet size to send over the network
