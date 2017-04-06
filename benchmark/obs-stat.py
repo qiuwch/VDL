032faa7cba7d6a28a044189520f729f31158a294
Weichao Qiu <qiuwch@gmail.com>
2017-04-05 23:12:03 -0400

    Add the observation info of different gym envs.

--- benchmark/obs-stat.py
+++ benchmark/obs-stat.py
@@ -1,10 +1,15 @@
 import gym
+env_names = ['CartPole-v0', 'Breakout-v0', 'Hopper-v1', 'Humanoid-v1']
+try:
+    import ppaquette_gym_doom
+    env_names.append('ppaquette/DoomBasic-v0')
+except:
+    pass
 
 def print_obs_info(env_name):
     env = gym.make(env_name)
     print env.observation_space
 
 if __name__ == '__main__':
-    env_names = ['CartPole-v0', 'Breakout-v0', 'Hopper-v1', 'Humanoid-v1']
     for v in env_names:
         print_obs_info(v)
