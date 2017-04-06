12feb4deb349106323f7f0ae81b03a6c23b508eb
Weichao Qiu <qiuwch@gmail.com>
2017-04-05 22:40:30 -0400

    Update training code.

--- arm-pose/train-cnn.py
+++ arm-pose/train-cnn.py
@@ -1,13 +1,17 @@
 # An CNN model with a L2 output layer to do regression on the arm parameters.
 # This network is modified from https://github.com/tensorflow/models/blob/master/tutorials/image/cifar10/cifar10_train.py
-import alexnet
+import alexnet # Load alexnet network structure
 import tensorflow as tf
-import varm
-
+import varm # Load synthetic training images
+import argparse
 
 def load_data():
     # Convert the rendered data
     return varm.load_data()
+
+def load_batch(batch_size = 50):
+    # return a batch  for training from the synthetic images
+    pass
 
 def inference_graph():
     return alexnet.inference_graph()
@@ -16,6 +20,43 @@ def loss_op(fc, labels):
     objective = tf.nn.l2_loss(fc)
 
     # Compute and apply gradient
+
+def compare(prediction, gt):
+    pass
+
+G.checkpoint_dir = 'tmp'
+
+def test(image, gt = None): # Apply the model to predict on an image
+    with tf.Session() as sess:
+        ckpt = tf.train.get_checkpoint_state(G.checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+        else:
+            print('No checkpoint file found')
+
+        # Run the testing on an image and output the estimation for this image
+        # Should be used for debug, demo
+
+        # Output the arm pose
+        inference_op = inference_graph()
+        # prediction = run(inference_op, image)
+        prediction = sess.run([inference_op])
+
+        # Apply inference op to the input image
+        if gt:
+            # Compare the prediction with gt
+            print compare(prediction, gt)
+
+        pass
+
+def eval():
+    # Evaluation on testing data
+    # Create an evaluation op and use session to run this op
+    pass
+
+def train_step(loss):
+    train_op = tf.train.AdamOptimizer(1e-4).minimize(loss)
+    return train_op
 
 def train():
     images, labels = load_data()
@@ -30,13 +71,30 @@ def train():
 
     train_op = train_step(loss)
 
-    with tf.train.MonitoredTrainingSession() as sess:
-        sess.run(train_op)
+    # with tf.train.MonitoredTrainingSession() as sess:
+    #     sess.run(train_op)
 
+    # Run this within a session
+    for i in range(20000):
+        batch_data = load_batch(50)
+        train_op.run(feed_dict = {image: batch_data[0], label: batch_data[1]})
 
 def main():
-    # Prepare dataset
-    train()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train', type=bool, help='Do training')
+    parser.add_argument('--test', type=bool, help='Do testing')
+
+    args = parser.parse_args()
+
+    if args.train():
+        # Prepare dataset
+        train()
+
+    if args.test():
+        test()
+
+def test_load_batch():
+    pass
 
 
 if __name__ == '__main__':
--- arm-pose/varm.py
+++ arm-pose/varm.py
@@ -1,11 +1,15 @@
 # Provides the split of training and validation data
+import json # The label is stored as a json file
+import cv2 # Read images
 
 
+# Split data into training, val and test
 def test_data():
     pass
 
-
 def train_data():
+    # Read image from hard disk and shuffle them
+
     [image_batch, label_batch] = tf.train.batch(
         [image, label],
         batch_size = batch_size,
