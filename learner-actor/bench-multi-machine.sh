5a33a887d676fb13bfbbbd3c186b65b489e13144
Weichao Qiu <qiuwch@gmail.com>
2017-04-09 16:28:00 -0400

    Update multi machine benchmark code.

--- learner-actor/bench-multi-machine.sh
+++ learner-actor/bench-multi-machine.sh
@@ -1,4 +1,5 @@
 # Star the learner
 python learner.py 2 Breakout-v0
-ssh n001 "source ~/.bash_profile; python actor.py yuille-fb-head 10000 Breakout-v0"
-ssh n002 "source ~/.bash_profile; python actor.py yuille-fb-head 10001 Breakout-v0"
+server=10.1.1.1
+ssh n001 "source ~/.bash_profile; python actor.py ${server} 10000 Breakout-v0"
+ssh n002 "source ~/.bash_profile; python actor.py ${server} 10001 Breakout-v0"
