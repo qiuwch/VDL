bd4dca294a6d0bcc0ffc7b3175e8aba33bbfaa43
Weichao Qiu <qiuwch@gmail.com>
2017-04-09 17:25:46 -0400

    Add example for ssh execute.

--- learner-actor/infinity_loop.py
+++ learner-actor/infinity_loop.py
@@ -4,3 +4,5 @@ while True:
     print 'Hello'
     time.sleep(1)
 
+# ssh n001 "python ${HOME}/workspace/VDL/VDL/learner-actor/infinity_loop.py"
+# ssh n001 -f "python ${HOME}/workspace/VDL/VDL/learner-actor/infinity_loop.py"
