ff268c6aa4f9cc030c32b867f566c62da5cc466d
Weichao Qiu <qiuwch@gmail.com>
2017-04-09 16:28:38 -0400

    Fix actor path.

--- learner-actor/bench-multi-machine.sh
+++ learner-actor/bench-multi-machine.sh
@@ -1,4 +1,9 @@
 # Star the learner
-python learner.py 2 Breakout-v0
-ssh n001 "source ~/.bash_profile; python actor.py yuille-fb-head 10000 Breakout-v0"
-ssh n002 "source ~/.bash_profile; python actor.py yuille-fb-head 10001 Breakout-v0"
+learner_bin=learner.py
+actor_bin=${HOME}/workspace/VDL/VDL/learner-actor/actor.py 
+echo "Start learner"
+python learner.py 2 Breakout-v0 &
+echo "Start actor1"
+ssh n001 "source ~/.bash_profile; python ${actor_bin} yuille-fb-head 10000 Breakout-v0"
+echo "Start actor2"
+ssh n003 "source ~/.bash_profile; python ${actor_bin} yuille-fb-head 10001 Breakout-v0"
