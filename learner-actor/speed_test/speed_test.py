a131f07e3ae585318d10602969fbfa878e2db031
Weichao Qiu <qiuwch@gmail.com>
2017-04-10 02:41:58 -0400

    Update speed test to support multi clients.

--- learner-actor/speed_test/speed_test.py
+++ learner-actor/speed_test/speed_test.py
@@ -1,6 +1,5 @@
 # A simple script to test the upper bound socket communication speed we can achieve
-import argparse
-import socket, sys
+import argparse, socket, sys, threading
 sys.path.append('..')
 from util import Timer, Counter
 import numpy as np
@@ -8,24 +7,41 @@ import numpy as np
 send_timer = Timer("Communication")
 throughput_counter = Counter("Network Throughput")
 
+class RecvThread(threading.Thread):
+    def __init__(self, connection):
+        super(RecvThread, self).__init__()
+        self.connection = connection
+
+    def run(self):
+        while True:
+            data = self.connection.recv(1000)
+            if not data:
+                print 'Done'
+                break
+
+
 port = 10010
-def server():
+def server(num_client):
     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listen_socket.bind(('', port))
     listen_socket.listen(5)
 
-    connection, addr = listen_socket.accept()
-    while True:
-        data = connection.recv(1000)
-        if not data:
-            print 'Done'
-            break
+    recv_threads = []
+    for i in range(num_client):
+        connection, addr = listen_socket.accept()
+        recv_threads.append(RecvThread(connection))
+
+    for thread in recv_threads:
+        thread.start()
+
+    for thread in recv_threads:
+        thread.join()
 
 def client(server_ip):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server_ip, port))
 
-    send(client_socket, 1000, 1000)
+    send(client_socket, 100000, 1000)
     client_socket.close()
 
 def send(client_socket, size, count):
@@ -40,15 +56,16 @@ def send(client_socket, size, count):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--server', action='store_true')
-    parser.add_argument('--client')
+    parser.add_argument('--num_client', type=int, default=1)
+    parser.add_argument('--client', action='store_true')
+    parser.add_argument('--server_ip', default='localhost')
     args = parser.parse_args()
 
     if args.server:
-        server()
+        server(args.num_client)
 
     if args.client:
-        server_ip = args.client
-        client(server_ip)
+        client(args.server_ip)
         print throughput_counter
         print send_timer
         print 'Speed: %.2fKB/s' % (throughput_counter.sum / send_timer.total / 1000)
