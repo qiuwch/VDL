ef277fe9a303755bcb6898fc33c720a6c9402ee0
Weichao Qiu <qiuwch@gmail.com>
2017-04-11 02:50:05 -0400

    Update benchmark script.

--- learner-actor/run_contour_data.sh
+++ learner-actor/run_contour_data.sh
@@ -6,9 +6,11 @@ port=10000
 run_task_fb () # Space is needed
 {
   server='10.1.1.1'
-  python learner.py --port 10000 --task ${task} &
-  ssh n001 -f "source ~/.bash_profile; python ${actor_bin} ${server} ${port} ${task}"
-  ssh n003 -f "source ~/.bash_profile; python ${actor_bin} ${server} ${port} ${task}"
+  python ${learner_bin} --port ${port} ${learner_args} &
+  actor_cmd="python ${actor_bin} ${actor_args} --server_ip ${server} --port ${port}"
+  ssh n001 -f "source ~/.bash_profile; ${actor_cmd}"
+  ssh n003 -f "source ~/.bash_profile; ${actor_cmd}"
+  wait $(jobs -p)
 }
 
 run_task_local ()
@@ -17,10 +19,11 @@ run_task_local ()
   echo "Start learner"
   python ${learner_bin} --port ${port} ${learner_args} &
   sleep 1 # Wait for the server to boot
+  actor_cmd="python ${actor_bin} --server_ip ${server} --port ${port} ${actor_args} &"
   echo "Start actor1"
-  python ${actor_bin} --server_ip ${server} --port ${port} ${actor_args} &
+  ${actor_cmd}
   echo "Start actor2"
-  python ${actor_bin} --server_ip ${server} --port ${port} ${actor_args} &
+  ${actor_cmd}
   wait $(jobs -p)
 }
 
@@ -42,9 +45,16 @@ run_baseline ()
 for task in CartPole-v0 Breakout-v0 Hopper-v1 Humanoid-v1
 # for task in ppaquette/DoomDeathmatch-v0
 do
-  # With compression
-  learner_args="--task ${task} --log log/${task}-nocompress-local.json"
-  actor_args="--task ${task}"
+  # 1. baseline
   run_baseline
-  run_task_local
+
+  # 2. no compress in fb
+  learner_args="--task ${task} --log log/${task}-nocompress-fb.json"
+  actor_args="--task ${task}"
+  run_task_fb
+
+  # 3. compress in fb
+  learner_args="--task ${task} --log log/${task}-compress-fb.json"
+  actor_args="--task ${task} --compress"
+  run_task_fb
 done
