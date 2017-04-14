d85d25258848f053e20e5b28c8cd37611a0c7e02
Weichao Qiu <qiuwch@gmail.com>
2017-04-14 14:57:12 -0400

    Add two scripts to test the CCVL machines.

---
+++ universe-starter-agent/install-script.sh
@@ -0,0 +1,8 @@
+# Prepare CCVL1-5 for with docker, tensorflow, etc.
+# Install docker
+
+# Install tensorflow
+
+# Install gym, universe, etc.
+
+pip install --user tensorflow gym
---
+++ universe-starter-agent/test.sh
@@ -0,0 +1,2 @@
+# Run a test script to make sure all dependencies have been properly installed.
+python train.py --num-workers 1 --env-id flashgames.NeonRace-v0 --log-dir /tmp/neonrace
