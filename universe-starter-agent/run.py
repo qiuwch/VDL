c54bc32b5e0050c2a064aeea8161fa8f1a119f22
Yuan Jing Vincent Yan <kaikulimu@yahoo.com.hk>
2017-04-16 17:28:31 -0400

    Learned how to run code w/o invoking tmux; summary in txt file

--- universe-starter-agent/run.py
+++ universe-starter-agent/run.py
@@ -84,3 +84,69 @@ def run(args, server):
     # Ask for all the services to stop.
     sv.stop()
     logger.info('reached %s steps. worker stopped.', global_step)
+
+def cluster_spec(num_workers, num_ps):
+    """
+More tensorflow setup for data parallelism
+"""
+    cluster = {}
+    port = 12222
+
+    all_ps = []
+    host = '127.0.0.1'
+    for _ in range(num_ps):
+        all_ps.append('{}:{}'.format(host, port))
+        port += 1
+    cluster['ps'] = all_ps
+
+    all_workers = []
+    for _ in range(num_workers):
+        all_workers.append('{}:{}'.format(host, port))
+        port += 1
+    cluster['worker'] = all_workers
+    return cluster
+
+def main(_):
+    """
+Setting up Tensorflow for data parallel work
+"""
+
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('-v', '--verbose', action='count', dest='verbosity', default=0, help='Set verbosity.')
+    parser.add_argument('--task', default=0, type=int, help='Task index')
+    parser.add_argument('--job-name', default="worker", help='worker or ps')
+    parser.add_argument('--num-workers', default=1, type=int, help='Number of workers')
+    parser.add_argument('--log-dir', default="/tmp/pong", help='Log directory path')
+    parser.add_argument('--env-id', default="PongDeterministic-v3", help='Environment id')
+    parser.add_argument('-r', '--remotes', default=None,
+                        help='References to environments to create (e.g. -r 20), '
+                             'or the address of pre-existing VNC servers and '
+                             'rewarders to use (e.g. -r vnc://localhost:5900+15900,vnc://localhost:5901+15901)')
+
+    # Add visualisation argument
+    parser.add_argument('--visualise', action='store_true',
+                        help="Visualise the gym environment by running env.render() between each timestep")
+
+    args = parser.parse_args()
+    spec = cluster_spec(args.num_workers, 1)
+    cluster = tf.train.ClusterSpec(spec).as_cluster_def()
+
+    def shutdown(signal, frame):
+        logger.warn('Received signal %s: exiting', signal)
+        sys.exit(128+signal)
+    signal.signal(signal.SIGHUP, shutdown)
+    signal.signal(signal.SIGINT, shutdown)
+    signal.signal(signal.SIGTERM, shutdown)
+
+    if args.job_name == "worker":
+        server = tf.train.Server(cluster, job_name="worker", task_index=args.task,
+                                 config=tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=2))
+        run(args, server)
+    else:
+        server = tf.train.Server(cluster, job_name="ps", task_index=args.task,
+                                 config=tf.ConfigProto(device_filters=["/job:ps"]))
+        while True:
+            time.sleep(1000)
+
+if __name__ == "__main__":
+    tf.app.run()
