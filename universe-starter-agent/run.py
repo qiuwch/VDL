8f31d025297eae50c32a972f6b2f38507245d386
Weichao Qiu <qiuwch@users.noreply.github.com>
2017-04-16 16:50:52 -0400

    Create run.py

---
+++ universe-starter-agent/run.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+import cv2
+import go_vncdriver
+import tensorflow as tf
+import argparse
+import logging
+import sys, signal
+import time
+import os
+from a3c import A3C
+from envs import create_env
+import distutils.version
+use_tf12_api = distutils.version.LooseVersion(tf.VERSION) >= distutils.version.LooseVersion('0.12.0')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Disables write_meta_graph argument, which freezes entire process and is mostly useless.
+class FastSaver(tf.train.Saver):
+    def save(self, sess, save_path, global_step=None, latest_filename=None,
+             meta_graph_suffix="meta", write_meta_graph=True):
+        super(FastSaver, self).save(sess, save_path, global_step, latest_filename,
+                                    meta_graph_suffix, False)
+
+def run(args, server):
+    env = create_env(args.env_id, client_id=str(args.task), remotes=args.remotes)
+    trainer = A3C(env, args.task, args.visualise)
+
+    # Variable names that start with "local" are not saved in checkpoints.
+    if use_tf12_api:
+        variables_to_save = [v for v in tf.global_variables() if not v.name.startswith("local")]
+        init_op = tf.variables_initializer(variables_to_save)
+        init_all_op = tf.global_variables_initializer()
+    else:
+        variables_to_save = [v for v in tf.all_variables() if not v.name.startswith("local")]
+        init_op = tf.initialize_variables(variables_to_save)
+        init_all_op = tf.initialize_all_variables()
+    saver = FastSaver(variables_to_save)
+
+    var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
+    logger.info('Trainable vars:')
+    for v in var_list:
+        logger.info('  %s %s', v.name, v.get_shape())
+
+    def init_fn(ses):
+        logger.info("Initializing all parameters.")
+        ses.run(init_all_op)
+
+    config = tf.ConfigProto(device_filters=["/job:ps", "/job:worker/task:{}/cpu:0".format(args.task)])
+    logdir = os.path.join(args.log_dir, 'train')
+
+    if use_tf12_api:
+        summary_writer = tf.summary.FileWriter(logdir + "_%d" % args.task)
+    else:
+        summary_writer = tf.train.SummaryWriter(logdir + "_%d" % args.task)
+
+    logger.info("Events directory: %s_%s", logdir, args.task)
+    sv = tf.train.Supervisor(is_chief=(args.task == 0),
+                             logdir=logdir,
+                             saver=saver,
+                             summary_op=None,
+                             init_op=init_op,
+                             init_fn=init_fn,
+                             summary_writer=summary_writer,
+                             ready_op=tf.report_uninitialized_variables(variables_to_save),
+                             global_step=trainer.global_step,
+                             save_model_secs=30,
+                             save_summaries_secs=30)
+
+    num_global_steps = 100000000
+
+    logger.info(
+        "Starting session. If this hangs, we're mostly likely waiting to connect to the parameter server. " +
+        "One common cause is that the parameter server DNS name isn't resolving yet, or is misspecified.")
+    with sv.managed_session(server.target, config=config) as sess, sess.as_default():
+        sess.run(trainer.sync)
+        trainer.start(sess, summary_writer)
+        global_step = sess.run(trainer.global_step)
+        logger.info("Starting training at step=%d", global_step)
+        while not sv.should_stop() and (not num_global_steps or global_step < num_global_steps):
+            trainer.process(sess)
+            global_step = sess.run(trainer.global_step)
+
+    # Ask for all the services to stop.
+    sv.stop()
+    logger.info('reached %s steps. worker stopped.', global_step)
