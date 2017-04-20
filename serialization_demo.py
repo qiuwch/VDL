a31da3b78fdbbef79b3f62849a4f85403b4bf468
Weichao Qiu <qiuwch@gmail.com>
2017-04-19 21:20:17 -0400

    Add a serialization demo for Kaiyue.

---
+++ serialization_demo.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cPickle
+
+def searialize_pickle(data):
+    bytearr = cPickle.dumps(data)
+    return bytearr
+
+def deserialize_pickle(bytearr):
+    data = cPickle.loads(bytearr)
+    return data
+
+
+
+obs = np.zeros((300, 300))
+data = dict(
+    obs = obs,
+    reward = 0,
+    terminal = False,
+    info = dict(extra_field=''),
+)
+
+print data
+
+bytearr = searialize_pickle(data)
+
+recovered_data = deserialize_pickle(bytearr)
+
+print recovered_data
