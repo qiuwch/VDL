39bb2dd6d9d3a4be5a3303e43b515036ddfc9ddb
Weichao Qiu <qiuwch@gmail.com>
2017-04-24 17:51:11 -0400

    Simplify varm training code.

---
+++ pytorch/utils.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
+import torch
+######################################################################
+# Visualize a few images
+# ^^^^^^^^^^^^^^^^^^^^^^
+# Let's visualize a few training images so as to understand the data
+# augmentations.
+
+def format_title(x):
+    return '\n'.join(['%.2f' % v for v in x])
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+def visualize_model(model, num_images=6):
+    images_so_far = 0
+    fig = plt.figure()
+
+    for i, data in enumerate(dset_loaders['val']):
+        inputs, labels = data
+        if use_gpu:
+            inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+        else:
+            inputs, labels = Variable(inputs), Variable(labels)
+
+        outputs = model(inputs)
+        _, preds = torch.max(outputs.data, 1)
+        # print('Label')
+        # print(labels)
+        # print('Prediction')
+        # print(outputs)
+        acc5 = metric(outputs, labels, 5)
+        acc10 = metric(outputs, labels, 10)
+        acc15 = metric(outputs, labels, 15)
+        print(outputs.size())
+        print(acc5, acc10, acc15)
+
+        for j in range(inputs.size()[0]):
+            images_so_far += 1
+            ax = plt.subplot(num_images//2, 2, images_so_far)
+            ax.axis('off')
+            ax.set_title('predicted: {}'.format(labels.data[j]))
+            imshow(inputs.cpu().data[j])
+
+            if images_so_far == num_images:
+                return
+
+def metric(prediction, label, threshold=5):
+    # only consider the first three dimensions
+    diff = (prediction - label).cpu().data.numpy()
+    acc = (abs(diff) < threshold).sum(axis=1)
+    return acc
--- pytorch/varm_tutorial.py
+++ pytorch/varm_tutorial.py
@@ -1,38 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Transfer Learning tutorial
-==========================
-**Author**: `Sasank Chilamkurthy <https://chsasank.github.io>`_
-
-In this tutorial, you will learn how to train your network using
-transfer learning. You can read more about the transfer learning at `cs231n
-notes <http://cs231n.github.io/transfer-learning/>`__
-
-Quoting this notes,
-
-    In practice, very few people train an entire Convolutional Network
-    from scratch (with random initialization), because it is relatively
-    rare to have a dataset of sufficient size. Instead, it is common to
-    pretrain a ConvNet on a very large dataset (e.g. ImageNet, which
-    contains 1.2 million images with 1000 categories), and then use the
-    ConvNet either as an initialization or a fixed feature extractor for
-    the task of interest.
-
-These two major transfer learning scenarios looks as follows:
-
--  **Finetuning the convnet**: Instead of random initializaion, we
-   initialize the network with a pretrained network, like the one that is
-   trained on imagenet 1000 dataset. Rest of the training looks as
-   usual.
--  **ConvNet as fixed feature extractor**: Here, we will freeze the weights
-   for all of the network except that of the final fully connected
-   layer. This last fully connected layer is replaced with a new one
-   with random weights and only this layer is trained.
-
-"""
-# License: BSD
-# Author: Sasank Chilamkurthy
-
 from __future__ import print_function, division
 
 import torch
@@ -43,35 +9,13 @@ import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
-import time
 import copy
-import os
+import os, time
+from utils import imshow, visualize_model, metric, format_title
+from varm_data import VArmDataset
 
 plt.ion()   # interactive mode
 
-######################################################################
-# Load Data
-# ---------
-#
-# We will use torchvision and torch.utils.data packages for loading the
-# data.
-#
-# The problem we're going to solve today is to train a model to classify
-# **ants** and **bees**. We have about 120 training images each for ants and bees.
-# There are 75 validation images for each class. Usually, this is a very
-# small dataset to generalize upon, if trained from scratch. Since we
-# are using transfer learning, we should be able to generalize reasonably
-# well.
-#
-# This dataset is a very small subset of imagenet.
-#
-# .. Note ::
-#    Download the data from
-#    `here <https://download.pytorch.org/tutorial/hymenoptera_data.zip>`_
-#    and extract it to the current directory.
-
-# Data augmentation and normalization for training
-# Just normalization for validation
 data_transforms = {
     'train': transforms.Compose([
         # transforms.RandomSizedCrop(224),
@@ -88,14 +32,12 @@ data_transforms = {
     ]),
 }
 
-# data_dir = 'hymenoptera_data'
-# dsets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
-#         for x in ['train', 'val']}
-from varm_data import VArmDataset
 
 dsets = dict(
     train = VArmDataset('data/train', transform=data_transforms['train']),
-    val = VArmDataset('data/val', transform=data_transforms['val'])
+    val = VArmDataset('data/val', transform=data_transforms['val']),
+    # Do not transform test data
+    test = VArmDataset('data/test', transform=data_transforms['val']),
 )
 dset_loaders = {x: torch.utils.data.DataLoader(dsets[x], batch_size=4,
                                                shuffle=True, num_workers=4)
@@ -105,49 +47,13 @@ dset_sizes = {x: len(dsets[x]) for x in ['train', 'val']}
 
 use_gpu = torch.cuda.is_available()
 
-
-######################################################################
-# Visualize a few images
-# ^^^^^^^^^^^^^^^^^^^^^^
-# Let's visualize a few training images so as to understand the data
-# augmentations.
-
-def imshow(inp, title=None):
-    """Imshow for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    plt.imshow(inp)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)  # pause a bit so that plots are updated
-
-
 # Get a batch of training data
 inputs, classes = next(iter(dset_loaders['train']))
 
 # Make a grid from batch
 out = torchvision.utils.make_grid(inputs)
 
-def format_title(x):
-    return '\n'.join(['%.2f' % v for v in x])
 imshow(out, title=[format_title(x) for x in classes])
-
-
-######################################################################
-# Training the model
-# ------------------
-#
-# Now, let's write a general function to train a model. Here, we will
-# illustrate:
-#
-# -  Scheduling the learning rate
-# -  Saving (deep copying) the best model
-#
-# In the following, parameter ``lr_scheduler(optimizer, epoch)``
-# is a function  which modifies ``optimizer`` so that the learning
-# rate is changed according to desired schedule.
 
 def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=25):
     since = time.time()
@@ -238,64 +144,10 @@ def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=7):
     return optimizer
 
 
-######################################################################
-# Visualizing the model predictions
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#
-# Generic function to display predictions for a few images
-#
-
-def visualize_model(model, num_images=6):
-    images_so_far = 0
-    fig = plt.figure()
-
-    for i, data in enumerate(dset_loaders['val']):
-        inputs, labels = data
-        if use_gpu:
-            inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
-        else:
-            inputs, labels = Variable(inputs), Variable(labels)
-
-        outputs = model(inputs)
-        _, preds = torch.max(outputs.data, 1)
-        # print('Label')
-        # print(labels)
-        # print('Prediction')
-        # print(outputs)
-        acc5 = metric(outputs, labels, 5)
-        acc10 = metric(outputs, labels, 10)
-        acc15 = metric(outputs, labels, 15)
-        print(outputs.size())
-        print(acc5, acc10, acc15)
-
-        for j in range(inputs.size()[0]):
-            images_so_far += 1
-            ax = plt.subplot(num_images//2, 2, images_so_far)
-            ax.axis('off')
-            ax.set_title('predicted: {}'.format(labels.data[j]))
-            imshow(inputs.cpu().data[j])
-
-            if images_so_far == num_images:
-                return
-
-def metric(prediction, label, threshold=5):
-    # only consider the first three dimensions
-    diff = (prediction - label).cpu().data.numpy()
-    acc = (abs(diff) < threshold).sum(axis=1)
-    return acc
-
-
 def evaluate_model():
     # The criteria for arm pose estimation?
     # The first three degrees. The camera degrees are not important
     pass
-
-######################################################################
-# Finetuning the convnet
-# ----------------------
-#
-# Load a pretrained model and reset final fully connected layer.
-#
 
 def main():
     model_ft = models.resnet18(pretrained=True)
@@ -312,34 +164,16 @@ def main():
     # Observe that all parameters are being optimized
     optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 
-    ######################################################################
-    # Train and evaluate
-    # ^^^^^^^^^^^^^^^^^^
-    #
-    # It should take around 15-25 min on CPU. On GPU though, it takes less than a
-    # minute.
-    #
-
     model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
                            num_epochs=25)
 
-    ######################################################################
-    #
-
     visualize_model(model_ft)
 
-
-    ######################################################################
-    # ConvNet as fixed feature extractor
-    # ----------------------------------
-    #
     # Here, we need to freeze all the network except the final layer. We need
     # to set ``requires_grad == False`` to freeze the parameters so that the
     # gradients are not computed in ``backward()``.
-    #
     # You can read more about this in the documentation
     # `here <http://pytorch.org/docs/notes/autograd.html#excluding-subgraphs-from-backward>`__.
-    #
 
     model_conv = torchvision.models.resnet18(pretrained=True)
     for param in model_conv.parameters():
@@ -369,16 +203,9 @@ def main():
     # This is expected as gradients don't need to be computed for most of the
     # network. However, forward does need to be computed.
     #
-
     model_conv = train_model(model_conv, criterion, optimizer_conv,
                              exp_lr_scheduler, num_epochs=25)
-
-
-    ######################################################################
-    #
     return model_conv
-
-
 
 if not os.path.isfile('arm.model'):
     model_conv = main()
